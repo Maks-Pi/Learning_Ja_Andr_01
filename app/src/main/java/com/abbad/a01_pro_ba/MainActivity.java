ef056a34c37861e5c97ea13b5f616be8af5fc378
Maks <maks_mask@ukr.net>
2021-01-07 21:10:09 +0200

    add some code. just for print.

--- app/src/main/java/com/abbad/a01_pro_ba/MainActivity.java
+++ app/src/main/java/com/abbad/a01_pro_ba/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import java.util.Scanner;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -15,7 +17,20 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void workNotHard() {
+        // новый пробный кусок кода. Смысла не имеет.
+        Float a;
+        Integer b;
+        Float ansver;
 
-        
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите дробь ");
+        a = sc.nextFloat();
+        Scanner sc1 = new Scanner(System.in);
+        System.out.println("Введите целое ");
+        b = sc1.nextInt();
+        ansver = a + b ;
+
+        System.out.println("ответ на ваш вопрос, о сумме двух значений: " + ansver);
+
     }
 }
