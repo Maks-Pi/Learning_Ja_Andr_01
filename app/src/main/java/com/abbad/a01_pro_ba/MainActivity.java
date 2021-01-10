43f9b472b4c0a90129eaecfdafdcf5fd75ad195d
Maks <maks_mask@ukr.net>
2021-01-10 23:46:41 +0200

    Version for not working code.

--- app/src/main/java/com/abbad/a01_pro_ba/MainActivity.java
+++ app/src/main/java/com/abbad/a01_pro_ba/MainActivity.java
@@ -2,48 +2,85 @@ package com.abbad.a01_pro_ba;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.location.GnssAntennaInfo;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
+import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.Scanner;
-
+import java.util.HashMap;
+import java.util.Map;
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-<<<<<<< HEAD
-        
-        forSecondCangess();
-=======
 
-        workNotHard();
+        class personality () {
+            View textPhone;
+            View name;
+        }
+
+
+        //инициализация кнопок/ интерфейса
+        Button bSave = findViewById(R.id.button);
+        Button bNew = findViewById(R.id.button2);
+        Button bFind = findViewById(R.id.button3);
+        View textPhone = findViewById(R.id.editTextPhone);
+        View  name = findViewById(R.id.editTextTextPersonName);
+
+        LinkedList <View> person = new LinkedList<>();
+        person.add( name, textPhone );
+
+//        bSave.setOnClickListener p
+
+//         void newPerson() {
+//            textPhone = null;
+//            name = null;
+//        bNew.setOnClickListener(v1 -> newPerson());
+//        };name
+
+        View.OnClickListener onClick0 = new View.OnClickListener() {
+            @Override
+            public void onClick(View v0) {
+                () -> setname
+                textPhone =  ;
+                ((View) v0).set.
+                //((Button) v0).setText("Новое поле для записи");
+//            }
+//        };
+//
+//        bNew.setOnClickListener(onClick0);
+
+
+        public static void main (Object[] args){
+            // Создание телефоннтой книги на основе мар, ключ = имя
+            HashMap <Object, Object> phoneBook = new HashMap<>();
+            phoneBook.put(name, textPhone);
+            // HashMap put (String Name, String textPhone)
+            bSave.setOnClickListener(v -> phoneBook());
+        }
+
+    }
+    public static void main (String[] args){
+        // Создание телефоннтой книги на основе мар, ключ = имя
+        HashMap <String, String> phoneBook = new HashMap<>();
+
+
+
+//            phoneBook.put(name, textPhone);
+        // HashMap put (String Name, String textPhone)
+        bSave.setOnClickListener(v0 -> phoneBook());
     }
 
-    private void workNotHard() {
-        // новый пробный кусок кода. Смысла не имеет.
-        Float a;
-        Integer b;
-        Float ansver;
+//    public static void main (String[] args){
+//        // Создание телефоннтой книги на основе мар, ключ = имя
+//        HashMap <String, String> phoneBook = new HashMap<>();
+//        phoneBook.put(nae, )
+//        // HashMap put (String Name, String textPhone)
+//    }
 
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Введите дробь ");
-        a = sc.nextFloat();
-        Scanner sc1 = new Scanner(System.in);
-        System.out.println("Введите целое ");
-        b = sc1.nextInt();
-        ansver = a + b ;
-
-        System.out.println("ответ на ваш вопрос, о сумме двух значений: " + ansver);
-
->>>>>>> New_First_Work
-    }
-
-    private void forSecondCangess() {
-    }
-    // Пробные действия с визуальной оболочкой.
-
-
-
-    
 }
