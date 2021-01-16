945f5420e5f116f8dc8a4e9293ecef9e22482f06
Maks <maks_mask@ukr.net>
2021-01-16 13:00:54 +0200

    Step one? for working prog.

--- app/src/main/java/com/abbad/a01_pro_ba/MainActivity.java
+++ app/src/main/java/com/abbad/a01_pro_ba/MainActivity.java
@@ -19,68 +19,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        class personality () {
-            View textPhone;
-            View name;
+        
         }
 
-
-        //инициализация кнопок/ интерфейса
-        Button bSave = findViewById(R.id.button);
-        Button bNew = findViewById(R.id.button2);
-        Button bFind = findViewById(R.id.button3);
-        View textPhone = findViewById(R.id.editTextPhone);
-        View  name = findViewById(R.id.editTextTextPersonName);
-
-        LinkedList <View> person = new LinkedList<>();
-        person.add( name, textPhone );
-
-//        bSave.setOnClickListener p
-
-//         void newPerson() {
-//            textPhone = null;
-//            name = null;
-//        bNew.setOnClickListener(v1 -> newPerson());
-//        };name
-
-        View.OnClickListener onClick0 = new View.OnClickListener() {
-            @Override
-            public void onClick(View v0) {
-                () -> setname
-                textPhone =  ;
-                ((View) v0).set.
-                //((Button) v0).setText("Новое поле для записи");
-//            }
-//        };
-//
-//        bNew.setOnClickListener(onClick0);
-
-
-        public static void main (Object[] args){
-            // Создание телефоннтой книги на основе мар, ключ = имя
-            HashMap <Object, Object> phoneBook = new HashMap<>();
-            phoneBook.put(name, textPhone);
-            // HashMap put (String Name, String textPhone)
-            bSave.setOnClickListener(v -> phoneBook());
-        }
-
-    }
-    public static void main (String[] args){
-        // Создание телефоннтой книги на основе мар, ключ = имя
-        HashMap <String, String> phoneBook = new HashMap<>();
-
-
-
-//            phoneBook.put(name, textPhone);
-        // HashMap put (String Name, String textPhone)
-        bSave.setOnClickListener(v0 -> phoneBook());
-    }
-
-//    public static void main (String[] args){
-//        // Создание телефоннтой книги на основе мар, ключ = имя
-//        HashMap <String, String> phoneBook = new HashMap<>();
-//        phoneBook.put(nae, )
-//        // HashMap put (String Name, String textPhone)
-//    }
 
 }
+
---
+++ app/src/main/java/com/abbad/a01_pro_ba/Persona.java
@@ -0,0 +1,68 @@
+package com.abbad.a01_pro_ba;
+
+public class Persona {
+
+    private  String firstName;
+    private  String secondName;
+    private  String family;
+    private  String mobileNumber;
+    private  String homeNumber;
+    private  String notice;
+
+    public Persona (String firstName, String secondName, String family,String mobileNumber, String homeNumber, String notice) {
+        this.firstName = firstName;
+        this.secondName = secondName;
+        this.family = family;
+        this.mobileNumber = mobileNumber;
+        this.homeNumber = homeNumber;
+        this.notice = notice;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getSecondName() {
+        return secondName;
+    }
+
+    public void setSecondName(String secondName) {
+        this.secondName = secondName;
+    }
+
+    public String getFamily() {
+        return family;
+    }
+
+    public void setFamily(String family) {
+        this.family = family;
+    }
+
+    public String getMobileNumber() {
+        return mobileNumber;
+    }
+
+    public void setMobileNumber(String mobileNumber) {
+        this.mobileNumber = mobileNumber;
+    }
+
+    public String getHomeNumber() {
+        return homeNumber;
+    }
+
+    public void setHomeNumber(String homeNumber) {
+        this.homeNumber = homeNumber;
+    }
+
+    public String getNotice() {
+        return notice;
+    }
+
+    public void setNotice(String notice) {
+        this.notice = notice;
+    }
+}
